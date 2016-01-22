86fd58124739199a6068eeefeb2baaa888094ad2
Ethan Piekarski <snapdragon1127@gmail.com>
2016-01-22 12:12:04 -0500

    update minigolf.py a bit

--- minigolf.py
+++ minigolf.py
@@ -1,16 +1,16 @@
 def g(c,p):
- c=c.split('\n');
- o=[0]*len(max(c,key=len));
+ c=c.split('\n')
+ o=[0]*len(max(c,key=len))
  for l in c:
   i=0
   for v in l:
 	if v!=" ":o[i]=v
 	i+=1
- t = {"_":1,"/":5,"\\":-4}
+ t={"_":1,"/":5,"\\":-4}
  for v in o:
 	if v=="U":break
 	p-=t[v]
-	if p < 1:return
+	if p<1:return
  return 1
 
 powr = 27
