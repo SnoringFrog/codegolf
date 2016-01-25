92c13e380f4df08bb403d47abd46cd8cf67283c9
Ethan Piekarski <snapdragon1127@gmail.com>
2016-01-25 16:47:52 -0500

    update minigolf

--- minigolf.py
+++ minigolf.py
@@ -1,28 +1,44 @@
 def g(c,p):
- c=c.split('\n')
- o=[0]*len(max(c,key=len))
- for l in c:
-  i=0
-  for v in l:
-	if v!=" ":o[i]=v
-	i+=1
- t={"_":1,"/":5,"\\":-4}
+ o=[''.join(x).split()[0] for x in zip(*c.split('\n'))]
+ t={"_":1,"/":5,"\\":-4,'0':0}
  for v in o:
 	if v=="U" or p<1:return p>0
 	p-=t[v]
 
+def prep_course(course):
+ course = '\n'.join(course.split('\n')[1:])
+ course = '\n'.join(course.split('\n')[:-1])
+ return course
+
 powr = 27
-course = """
-      ____       ____ _
-   __/    \     /    U \ 
+course = prep_course("""
+____  _ 
+    \/ U
+""")
+print g(course,powr) #True
+course = prep_course("""
+      ____       ____ _   
+   __/    \     /    U \  
 __/        \   /        \_
-            \_/
-"""
-print g(course,powr)
-course = """
-      ____       ////____ _
-   __/    \     /    U \ 
+            \_/           
+""")
+print g(course,powr) #True
+
+course = prep_course("""
+      ____       //// _   
+   __/    \     /    U \  
 __/        \   /        \_
-            \_/
-"""
-print g(course,powr)
+            \_/           
+""")
+print g(course,powr) #False
+
+course = prep_course("""
+_____//\\\\\U
+""")
+print g(course,powr) #True
+
+powr=15 
+print g(course,powr) #false 
+
+powr=30 
+print g(course,powr) #true
